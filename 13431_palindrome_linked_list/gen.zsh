b5a46e178943d0bdd0bad6d2c9ca0f1ed3d509a5
nevikw39 <nevik.w39@gmail.com>
2022-02-02 11:33:00 +0800

    Partial Judge

--- 13431_palindrome_linked_list/gen.zsh
+++ 13431_palindrome_linked_list/gen.zsh
@@ -1,6 +1,6 @@
 #!/usr/bin/zsh
 
 for i in $(seq 1 4); do
-    echo $i | ./gen.py | tee testcases/in$i.txt | time ./sol > testcases/out$i.txt;
-    diff <(cat testcases/in$i.txt | ./sol.py) testcases/out$i.txt;
+    echo $i | ./gen.py | tee testcases/in$i.txt | time ./judge > testcases/out$i.txt;
+    diff <(cat testcases/in$i.txt | ./sln.py) testcases/out$i.txt;
 done;
