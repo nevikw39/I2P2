2598cfd57997adeff5dc73059a29d97be2b99704
nevikw39 <nevikw39@icloud.com>
2022-03-01 22:14:43 +0800

    Update testdata

--- 13431_palindrome_linked_list/gen.py
+++ 13431_palindrome_linked_list/gen.py
@@ -6,13 +6,11 @@ N = 10 ** int(input())
 for _ in range(rand(1, 10 ** 6 // N)):
     n = rand(1, N)
     print(n)
-    p = rand(2)
-    if p:
-        lst = [str(rand(-2**31, 2**31)) for _ in range(n // 2)]
-        if n & 1:
-            lst += [str(rand(-2**31, 2**31))] + lst[::-1]
-        else:
-            lst += lst[::-1]
-        print(' '.join(lst))
+    lst = [str(rand(-2**31, 2**31)) for _ in range(n // 2)]
+    if n & 1:
+        lst += [str(rand(-2**31, 2**31))] + lst[::-1]
     else:
-        print(' '.join([str(rand(-2**31, 2**31)) for _ in range(n)]))
+        lst += lst[::-1]
+    if rand(2):
+        lst[rand(n)] = str(rand(-2**31, 2**31))
+    print(' '.join(lst))
