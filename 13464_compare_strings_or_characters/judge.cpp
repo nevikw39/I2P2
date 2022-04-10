0533ec6b95d516bb583aff037db1541217b99a0c
nevikw39 <nevikw39@icloud.com>
2022-04-10 18:26:52 +0800

    Remove unnecessary namesapces

--- 13464_compare_strings_or_characters/judge.cpp
+++ 13464_compare_strings_or_characters/judge.cpp
@@ -1,16 +1,13 @@
 #include "function.h"
 #include <bits/extc++.h>
-// #pragma GCC optimize("Ofast,unroll-loops,no-stack-protector,fast-math")
-// #pragma GCC target("tune=native")
+#pragma GCC optimize("Ofast,unroll-loops,no-stack-protector,fast-math")
+#pragma GCC target("tune=native")
 #pragma message("GL; HF!")
 using namespace std;
-using namespace __gnu_cxx;
-using namespace __gnu_pbds;
 
 int main()
 {
-    ios::sync_with_stdio(false);
-    cin.tie(0);
+    cin.tie(nullptr)->sync_with_stdio(false);
     string s, t;
     int n;
     while (cin >> s >> t >> n)
